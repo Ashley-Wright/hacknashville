e61d08c28c2ef610b70feeedab15f0f70b32c241
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-05-03 11:40:26 -0500

    schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140503153829) do
+ActiveRecord::Schema.define(version: 20140503153952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: true do |t|
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email_address"
+    t.datetime "create_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "mentees", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -31,20 +41,5 @@ ActiveRecord::Schema.define(version: 20140503153829) do
 
   add_index "mentees", ["email"], name: "index_mentees_on_email", unique: true, using: :btree
   add_index "mentees", ["reset_password_token"], name: "index_mentees_on_reset_password_token", unique: true, using: :btree
-end
 
-ActiveRecord::Schema.define(version: 20140503153952) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "admins", force: true do |t|
-    t.string   "username"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email_address"
-    t.datetime "create_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 end
